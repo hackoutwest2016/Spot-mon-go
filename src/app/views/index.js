f654eceb39aa28f720306adf0186cecdf9fb2873
Kalle Hansson <kh.hansson@gmail.com>
2016-08-09 13:33:58 +0200

    SASS support added

--- src/app/views/index.js
+++ src/app/views/index.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import Menu from '../components/menu/index';
+import style from './main.scss';
 
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, world</h1>
+		<div>
+      		<h1>Hello, world</h1>
+	  		<Menu />
+		</div>
     );
   }
 }
