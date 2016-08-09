b534b0644483689506f42c04ac48a8308ebd1aa5
Kalle Hansson <kh.hansson@gmail.com>
2016-08-09 20:39:34 +0200

    Router fixed, global Menu and Popups created

--- src/app/components/challange/index.js
+++ src/app/components/challange/index.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 import style from './challange.scss';
 
 export default class Battle extends Component {
+	constructor () {
+		super();
+
+		this.state = {
+			open: true
+		}
+	}
+
   	render() {
     	return (
 			<div className="popup-container">
 				<div className="popup">
 					<p>A Spotémon manager has challanged you!</p>
-					<button class="popup-btn popup-btn-accept">Accept</button>
-					<button class="popup-btn popup-btn-decline">Decline</button>
+					<button className="popup-btn popup-btn-accept">Accept</button>
+					<button className="popup-btn popup-btn-decline">Decline</button>
 				</div>
 			</div>
 	    );
