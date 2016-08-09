16926179d3a306442417b5562be5ce482d63eda1
Emma Gustafsson <emma.i.gustafsson@gmail.com>
2016-08-09 22:33:17 +0200

    added css to spotemonview

--- src/app/views/spotemon/index.js
+++ src/app/views/spotemon/index.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react';
 import $ from 'jquery';
 import spot from './spotemon_2';
 import style from './spotemon.scss';
+import ProgressBar from 'progressbar.js';
 
 export default class Spotemon extends Component {
   	render() {
     	return (
     		<div id="spotemon">
-			    <h3>CP: {this.state.initialized ? this.state.carismaPoints : 'Loading'}</h3>
+			    <div className = "cp-text">CP: </div>
+			    <div className = "cp-points">{this.state.initialized ? this.state.carismaPoints : 'Loading'}</div>
+    			<div id = "container"></div>
     			<img src = {this.state.image} alt = "Artist Icon" /> 
 			    <div id="results">
-			    	<h4>Name:</h4>
-			    	<p>{this.state.initialized ? this.state.artistName : 'Loading'}</p>
+			    	<div className = "name">Name: </div>
+			    	<div className = "nameId">{this.state.initialized ? this.state.artistName : 'Loading'}</div>
 			    </div>
 			</div>
 	    );
@@ -27,6 +30,7 @@ export default class Spotemon extends Component {
 			artistId: null
 		}
 	  }
+
 	componentDidMount(){
 		var self = this;
 
@@ -34,7 +38,6 @@ export default class Spotemon extends Component {
 		self.setState(Object.assign({}, self.state, {artistId: artistId}));
 
 		var audio = new Audio();
-
 		spot.getArtistTopTracks(artistId,function (response) {
 			spot.getTrackToPlay(response.tracks[1].id, function(response){
 				self.setState(Object.assign({}, self.state, {artistName: response.artists[0].name, initialized:true, topTrack: response.name})),
@@ -43,12 +46,23 @@ export default class Spotemon extends Component {
 
 			})
 		})
+		
 
 		spot.getArtistPopularity(artistId,function (response) {
 			self.setState(Object.assign({}, self.state, {carismaPoints: response.popularity, image: response.images[1].url}));
+
 		})
 
+		var bar = new ProgressBar.SemiCircle(container, {
+			strokeWidth: 2,
+			easing: 'easeInOut',
+			duration: 1300,
+			color: '#FFEA82',
+			trailColor: '#eee',
+			trailWidth: 1,
+			svgStyle: null
+		});
+		bar.animate(0.67);
 	}
-	
 	
 };
