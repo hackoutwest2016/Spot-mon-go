59d9cbdca96715f539bf226503baedc609956b39
Emma Gustafsson <emma.i.gustafsson@gmail.com>
2016-08-10 08:48:05 +0200

    user view showing dummy values

--- src/app/views/user/index.js
+++ src/app/views/user/index.js
@@ -8,11 +8,13 @@ export default class User extends Component {
     	return (
 			<div id = "user">
     			<img src = {require('../../assets/images/user.png')} alt = "User Icon" /> 
-
+				<hr/>
 				<div className = "name">Name: </div>
 				<div className = "userName"> {this.state.initialized ? this.state.userName : 'Loading'}</div>
-				<div className = "recordLabel">{this.state.initialized ? this.state.recordLabel : 'Loading'}</div>
-				<div className = "spotemonFetched"> Antal Spotémon: {this.state.initialized? this.state.spotemonsFetched : 'Loading'}</div>
+				<br/>
+				<div className = "recordLabel">Record label: </div> <div className = "recordLabelName"> {this.state.initialized ? this.state.recordLabel : 'Loading'}</div>
+				<br/>
+				<div className = "spotemonFetched"> Antal spotémon fångade: </div> <div className ="spotemonData"> {this.state.initialized? this.state.spotemonsFetched : 'Loading'}</div>
 			</div>
 	    );
   	}
@@ -21,8 +23,13 @@ export default class User extends Component {
 	  	this.state = {
 			initialized: false,
 			userName: 'Lotta',
-			recordLabel: 'No Label chosen',
+			recordLabel: 'No label chosen yet',
 			spotemonsFetched: 0
 		}
-	  }
+	 }
+	 componentDidMount(){
+		var self = this;
+		this.setState(Object.assign({}, self.state, {initialized:true}));
+	 }
+
 }
