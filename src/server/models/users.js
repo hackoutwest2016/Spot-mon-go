338c32807f844274b8105b86361b0c7ea9fc7b64
Oskar Nyberg <bruno@oskarnyberg.com>
2016-08-10 18:34:29 +0200

    Added user counter

--- src/server/models/users.js
+++ src/server/models/users.js
@@ -1,5 +1,25 @@
 var tools = require('./tools');
 
+let max = 0;
+
+setInterval(cleanUsers, 10000);
+setTimeout(() => {
+    setInterval(analyzeUsers, 10000);
+}, 1000)
+
+function cleanUsers() {
+    let now = new Date().getTime();
+    state.users = state.users.filter(user => {
+        return user.timestamp + 30000 > now;
+    });
+}
+
+function analyzeUsers() {
+    let length = state.users.length;
+    max = Math.max(length, max);
+    console.log('current:', length, ', max:', max);
+}
+
 function getAllUsers () {
 	return state.users;
 }
@@ -19,19 +39,25 @@ function getCloseUsers (user) {
 
 function updateUser (userId, position) {
 	let found = false;
+    let timestamp = new Date().getTime();
+    let thisUser;
 
 	state.users.forEach(function(user){
 		if(user.id === userId){
+            thisUser = user;
 			user.coords = position;
+            user.timestamp = timestamp
 			found = true;
 		}
 	});
 
 	if(!found){
-		state.users.push({
+        thisUser = {
 			id: userId,
-			coords: position
-		});
+			coords: position,
+            timestamp: timestamp
+		};
+		state.users.push(thisUser);
 	}
 
 	return "success";
